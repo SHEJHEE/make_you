fd1b457e77b0548c25151e8e246b1e4d5a00848c
jhee <urijudy@naver.com>
2016-04-25 15:51:49 +0900

    change views

--- han/admin.py
+++ han/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import contents
+from .models import templates
 # Register your models here.
+#admin.site.register(make_U_models.Contents)
+
+admin.site.register(contents)
+admin.site.register(templates)
--- han/views.py
+++ han/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from .models import contents
 
 # Create your views here.
@@ -9,7 +10,35 @@ def main_page(request):
     return render(request, 'main_page.html')
 
 def agree_page(request):
+    if request.method =="POST":
+
+        pass
+
+
+
     return render(request, 'agree_page.html')
 
+
 def edit_page(request):
-    pass
+    if request.method == "POST":
+        new_contents = contents(
+            summary = request.POST.get('summary'),
+            workexp = request.POST.get('work_exp'),
+        )
+        new_contents = request.user
+        new_contents.save()
+
+        return redirect(select_template)
+
+
+    return render(request,'edit_page.html')
+
+def select_template(request):
+    new_contents = contents.objects.get(id==1)
+
+    ctx={
+        'contents' : new_contents,
+    }
+
+    return render(request, 'select_template.html',ctx)
+
